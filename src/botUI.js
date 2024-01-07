cbf44d37b0fa4ea8b2b03ea0cbf065d10cadb6bd
lokesh Inumpudi <lokesh.inumpudi@games24x7.com>
2024-01-07 17:57:50 +0530

    asd

--- src/botUI.js
+++ src/botUI.js
@@ -46,7 +46,7 @@ function BotUi({ topicData }) {
 
   return (
     <div>
-      <div className="fixed pt-0 mt-8 bottom-6 w-full inline-flex items-center justify-center ">
+      <div className="fixed pt-0 ml-4 mt-8 bottom-6 w-full inline-flex items-center justify-center ">
         <form
           className=""
           onSubmit={(e) => {
@@ -56,7 +56,7 @@ function BotUi({ topicData }) {
         >
           <div className="flex flex-wrap ">
             <textarea
-              className="flex h-28 w-full rounded-md border border-[#e5e7eb] px-3 py-2 text-sm placeholder-[#6b7280] focus:outline-none focus:ring-2 focus:ring-[#9ca3af] disabled:cursor-not-allowed disabled:opacity-50 text-[#030712] focus-visible:ring-offset-2"
+              className="flex h-28 w-96 rounded-md border border-[#e5e7eb] px-4 py-2 text-sm placeholder-[#6b7280] focus:outline-none focus:ring-2 focus:ring-[#9ca3af] disabled:cursor-not-allowed disabled:opacity-50 text-[#030712] focus-visible:ring-offset-2"
               placeholder="Type your message"
               value={userInput}
               onChange={(e) => {
@@ -64,7 +64,7 @@ function BotUi({ topicData }) {
               }}
             />
             <button
-              className="inline-flex w-full  items-center justify-center rounded-md text-sm font-medium text-[#f9fafb] disabled:pointer-events-none disabled:opacity-50 bg-black hover:bg-[#111827E6] h-10 px-4 py-2"
+              className="inline-flex w-96 items-center justify-center rounded-md text-sm font-medium text-[#f9fafb] disabled:pointer-events-none disabled:opacity-50 bg-black hover:bg-[#111827E6] h-10 px-4 py-2"
               onClick={sendToAI}
             >
               Send
